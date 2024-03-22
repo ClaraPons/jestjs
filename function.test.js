2f0c19a18c8348fa09517c2555ad58f46c7ee0af
ClaraPons <claragapons@gmail.com>
2024-03-22 17:05:40 +0100

    add change in function test name

--- function.test.js
+++ function.test.js
@@ -4,6 +4,6 @@ test('Adds 2 + 2 equals 4', () => {
     expect(functions.add(2,2)).toBe(4);
 })
 
-test('Adds 2 + 2 equals 4', () => {
+test('Adds 2 + 2 not equals 5', () => {
     expect(functions.add(2,2)).not.toBe(5);
 })
